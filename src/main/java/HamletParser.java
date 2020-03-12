7d6216aac528049711ef060b77d9b6cade894216
vle <obentoka@gmail.com>
2020-03-11 23:50:27 -0400

    done

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,9 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +39,76 @@ public class HamletParser {
         return hamletData;
     }
 
+    public String findHameletAndReplace(String inputString){
+        String[] words = inputString.split(" ");
+        StringBuilder retString = new StringBuilder();
+        for(int i = 0; i < words.length; i++){
+            retString.append(changeHamletToLeon(words[i]));
+            if(i != words.length-1)
+                retString.append(" ");
+        }
+        return retString.toString();
+    }
+
+    public String findHoratioAndReplace(String inputString){
+        String[] words = inputString.split(" ");
+        StringBuilder retString = new StringBuilder();
+        for(int i = 0; i < words.length; i++){
+            retString.append(changeHoratioToTariq(words[i]));
+            if(i != words.length-1)
+                retString.append(" ");
+        }
+        return retString.toString();
+    }
+
+    public String changeHamletToLeon(String inputString){
+        String leon = "leon";
+        Pattern hamlet = Pattern.compile("[hH][aA][mM][lL][eE][tT]");
+        Matcher m = hamlet.matcher(inputString);
+        if(m.matches()){
+            if(inputString.charAt(1) == 'A')
+                leon = leon.toUpperCase();
+            else if(inputString.charAt(0) == 'H')
+                leon = "Leon";
+            return leon;
+        }
+        return inputString;
+    }
+
+    public String changeHoratioToTariq(String inputString){
+        String tariq = "tariq";
+        Pattern hamlet = Pattern.compile("[hH][oO][rR][aA][tT][iI][oO]");
+        Matcher m = hamlet.matcher(inputString);
+        if(m.matches()){
+            if(inputString.charAt(1) == 'O')
+                tariq = tariq.toUpperCase();
+            else if(inputString.charAt(0) == 'H')
+                tariq = "Tariq";
+            return tariq;
+        }
+        return inputString;
+    }
+
+    public void parseLeonTariq(File fileParsingTo) throws IOException {
+        String retString = "";
+        FileWriter fw = new FileWriter(fileParsingTo);
+        Pattern hamlet = Pattern.compile("Hamlet");
+        Matcher ham = hamlet.matcher(hamletData);
+        retString = ham.replaceAll("Leon");
+
+        Pattern hamlet2 = Pattern.compile("HAMLET");
+        ham = hamlet2.matcher(retString);
+        retString = ham.replaceAll("LEON");
+
+        Pattern horatio = Pattern.compile("Horatio");
+        ham = horatio.matcher(retString);
+        retString = ham.replaceAll("Tariq");
+
+        Pattern horatio2 = Pattern.compile("HORATIO");
+        ham = horatio2.matcher(retString);
+        retString = ham.replaceAll("TARIQ");
+
+        fw.write(retString);
+        fw.close();
+    }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class HamletParserTest {
@@ -14,18 +17,116 @@ public class HamletParserTest {
     }
 
     @Test
-    public void testChangeHamletToLeon() {
+    public void leonTariqTest() throws IOException {
+        File fileToWrite = new File("/Users/vle/Documents/Projects/Week6/RegexHameletParser_3.11/Maven.Regex-Hamlet-Parser/src/main/resources/leon.txt");
+        hamletParser.parseLeonTariq(fileToWrite);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String testHam = "Horatio";
+        String expected = "Tariq";
+        String actual = hamletParser.changeHoratioToTariq(testHam);
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void testFindHoratio() {
+    public void testChangeHoratioToTariq2() {
+        String testHam = "HORATIO";
+        String expected = "TARIQ";
+        String actual = hamletParser.changeHoratioToTariq(testHam);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangeHoratioToTariq3() {
+        String testHam = "Horati0o";
+        String expected = "Horati0o";
+        String actual = hamletParser.changeHoratioToTariq(testHam);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangeHamletToLeon() {
+        String testHam = "Hamlet";
+        String expected = "Leon";
+        String actual = hamletParser.changeHamletToLeon(testHam);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangeHamletToLeon2() {
+        String testHam = "HAMLET";
+        String expected = "LEON";
+        String actual = hamletParser.changeHamletToLeon(testHam);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangeHamletToLeon3() {
+        String testHam = "hamleet";
+        String expected = "hamleet";
+        String actual = hamletParser.changeHamletToLeon(testHam);
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHamlet() {
+        String test = "Is this hamlet a good test";
+        String expected = "Is this leon a good test";
+        String actual = hamletParser.findHameletAndReplace(test);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHamlet2() {
+        String test = "Is this Hamlet a good test";
+        String expected = "Is this Leon a good test";
+        String actual = hamletParser.findHameletAndReplace(test);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHamlet3() {
+        String test = "Is this homlet a good test";
+        String expected = "Is this homlet a good test";
+        String actual = hamletParser.findHameletAndReplace(test);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHoratio(){
+        String test = "Hamlet bad, Horatio good";
+        String expected = "Hamlet bad, Tariq good";
+        String actual = hamletParser.findHoratioAndReplace(test);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHoratio2(){
+        String test = "Hamlet bad, HORATIO good";
+        String expected = "Hamlet bad, TARIQ good";
+        String actual = hamletParser.findHoratioAndReplace(test);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindHoratio3(){
+        String test = "Hamlet bad, DEW good";
+        String expected = "Hamlet bad, DEW good";
+        String actual = hamletParser.findHoratioAndReplace(test);
+
+        assertEquals(expected, actual);
     }
 }
